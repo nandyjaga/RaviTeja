98d7afabd344ec92e15cdf8310b1488c4d82585e
nandyjaga <nandy.jaga@gmail.com>
2014-11-18 19:43:02 +0530

    Added search Module
    Added search Module

--- Example/js/MainMenuCtrl.js
+++ Example/js/MainMenuCtrl.js
@@ -7,4 +7,15 @@ rmipControllers.controller('MainMenuCtrl', ['$scope', '$http','$rootScope',
 		$scope.data.showBookMark=!$scope.data.showBookMark;
 		$(document).trigger( "evtShowBookMark", [ {showBookMark:$scope.data.showBookMark} ] )
 	}
+	$("#GblSearchBtn").on("click",function(){
+		$("#recentlyViewed").hide();
+		$("#searchAll").show();
+		$("#searchMenu").show();
+		$(".cls_all").show();
+	});
+	$(".cls_search_btn").on("click",function(){
+		$(".cls_all").hide();
+		$($(this).attr("data-target")).show();
+	});
+	
 }]);
