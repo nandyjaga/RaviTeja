d43e767cbe6a02e0d02a9235e3b9be2810c586f1
nandyjaga <nandy.jaga@gmail.com>
2014-12-01 20:51:47 +0530

    Added group selection for Chat
    Added group selection for Chat

--- Example/js/SideMenuCtrl.js
+++ Example/js/SideMenuCtrl.js
@@ -9,6 +9,11 @@ rmipControllers.controller('SideMenuCtrl', ['$scope', '$http','$rootScope',
 	 $(".cls_childItems").hide();
 	 
 	 $("#openChat").on("click",function(evt){
+		$('#chatGroupSelection').modal('show');
+		//window.open("chat1.html",'Chat','directories=no,titlebar=no,toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=700,height=800');
+	 });
+	 $("#btn_open_chat").on("click",function(evt){
+		$('#chatGroupSelection').modal('hide');
 		window.open("chat1.html",'Chat','directories=no,titlebar=no,toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=700,height=800');
 	 });
 	 $("#openChatAdmin").on("click",function(evt){
